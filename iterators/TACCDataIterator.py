ef17cd331ca3b403d2ae1017a5d8ec9c7fa13979
Alex Rosenfeld <alexbrosenfeld@gmail.com>
2018-07-01 19:21:57 -0500

    Fixed random bounds.

--- iterators/TACCDataIterator.py
+++ iterators/TACCDataIterator.py
@@ -13,13 +13,15 @@ from main_network.constants.EngFicGooBooks import EngFicGooBooks
 
 
 class TACCDataIterator(DataTterator):
-    def __init__(self):
-        super().__init__()
+    """Ignore this class. I'm using it as a way to test some of the functions with real data.
+    """
+    def __init__(self, batch_size):
+        super().__init__(batch_size)
         lbl = "overall_redo"
         constants = EngFicGooBooks()
         self.dataiter = GoogleBooksGenerator(lbl, constants, 5, 10000, start_year=1900, end_year=2009)
 
     def get_batch(self):
-        #returns 4 1-d numpy arrays of shape (batch_size,)
-        #target_ids, context_ids, times (scaled between 0 and 1), labels (1 for positive sample, 0 for negative sample)
+        # returns 4 1-d numpy arrays of shape (batch_size,)
+        # target_ids, context_ids, times (scaled between 0 and 1), labels (1 for positive sample, 0 for negative sample)
         return self.dataiter.get_batch()
