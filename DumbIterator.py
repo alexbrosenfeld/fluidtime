029287120e15fdb514b40b0f10d60d481f34afd6
Alex Rosenfeld <alexbrosenfeld@gmail.com>
2018-07-01 19:03:26 -0500

    Created DumbIterator.

--- DumbIterator.py
+++ DumbIterator.py
@@ -7,9 +7,9 @@ class DumbIterator(DataTterator):
         self.vocab_size = vocab_size
 
     def get_batch(self):
-        target_indices = [random.randint(self.vocab_size) for _ in range(self.batch_size)]
-        context_indices = [random.randint(self.vocab_size) for _ in range(self.batch_size)]
+        target_indices = [random.randint(0, self.vocab_size) for _ in range(self.batch_size)]
+        context_indices = [random.randint(0, self.vocab_size) for _ in range(self.batch_size)]
         times = [random.random() for _ in range(self.batch_size)]
-        labels = [random.randint(1) for _ in range(self.batch_size)]
+        labels = [random.randint(0, 2) for _ in range(self.batch_size)]
         return target_indices, context_indices, times, labels
 
