ea23015f1f2b68b3cf11cc18931cfafbbe0babcd
Alex Rosenfeld <alexbrosenfeld@gmail.com>
2019-11-27 01:46:38 -0500

    Annotate DataIterator.

--- iterators/DataIterator.py
+++ iterators/DataIterator.py
@@ -2,6 +2,17 @@ from utils.time_tool import year2dec, dec2year
 
 
 class DataIterator(object):
+    """Base class for training data iterators.
+
+        Arguments:
+            args: argparse, config options
+
+        Properties:
+            batch_size: int, size of batch
+            start_year: int, start year
+            end_year: int, end year
+        """
+
     def __init__(self, args):
         self.args = args
         self.batch_size = args.batch_size
@@ -9,11 +20,13 @@ class DataIterator(object):
         self.end_year = args.end_year
         pass
 
+    # These functions convert years to number between 0 and 1 (and vice versa)
     def year2dec(self, year):
         return year2dec(year, self.args)
 
     def dec2year(self, dec):
         return dec2year(dec, self.args)
 
+    # Each call returns a sngle training batch.
     def get_batch(self):
         raise NotImplementedError
